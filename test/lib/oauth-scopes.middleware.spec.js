21f7a7a85d13431fc34eb23ed2973f10a83caa56
Igor Savin <iselwin@gmail.com>
2019-04-05 02:07:50 +0200

    Migrate to using middleware-testlab

--- test/lib/oauth-scopes.middleware.spec.js
+++ test/lib/oauth-scopes.middleware.spec.js
@@ -1,85 +1,111 @@
-const express = require("express");
+const yaml = require('js-yaml')
+const fs = require('fs')
+const request = require('supertest')
+const assert = require('chai').assert
+const { newExpressApp } = require('middleware-testlab')
 
-const request = require("supertest");
-const assert = require("chai").assert;
+const swaggerFile = fs.readFileSync('test/swagger/swagger.yaml')
+const swaggerDocument = yaml.safeLoad(swaggerFile)
+const oauthScopes = require('../../lib/oauth-scopes.middleware')
+const extractor = require('../extractors/simple.scopes.extractor')
 
-const appHelper = require("../helpers/test.app.helper");
-require("../helpers/test.bootstrap");
+describe('oauth-scopes.middleware', () => {
+  it('accepts if scopes are exact match for root endpoint', () => {
+    const app = newExpressApp({
+      middleware: oauthScopes(swaggerDocument, extractor),
+      endpoint: '/'
+    })
 
-describe("oauth-scopes.middleware", () => {
-  let app;
-  before(() => {
-    app = express();
-    app.use("/", require("../controllers/test.controller"));
-
-    app.use((err, req, res, next) => {
-      res.status(500).json({ details: err.message });
-    });
-
-    return appHelper.launchApp(app);
-  });
-
-  it("accepts if scopes are exact match for root endpoint", () => {
     return request(app)
-      .get("/")
+      .get('/')
       .query({
-        scopes: ["read:root"]
+        scopes: ['read:root']
       })
-      .expect(200);
-  });
+      .expect(204)
+  })
 
-  it("accepts if scopes are exact match for non-root endpoint", () => {
+  it('accepts if scopes are exact match for non-root endpoint', () => {
+    const app = newExpressApp({
+      middleware: oauthScopes(swaggerDocument, extractor),
+      endpoint: '/doc'
+    })
+
     return request(app)
-      .get("/doc")
+      .get('/doc')
       .query({
-        scopes: ["read:doc"]
+        scopes: ['read:doc']
       })
-      .expect(200);
-  });
+      .expect(204)
+  })
 
-  it("supports explicit mapping of middleware to path", () => {
+  it('supports explicit mapping of middleware to path', () => {
+    const app = newExpressApp({
+      middleware: oauthScopes(swaggerDocument, extractor, '/explicitly-doc'),
+      endpoint: '/doc-explicit'
+    })
+
     return request(app)
-      .get("/doc-explicit")
+      .get('/doc-explicit')
       .query({
-        scopes: ["read:doc"]
+        scopes: ['read:doc']
       })
-      .expect(200);
-  });
+      .expect(204)
+  })
 
-  it("accepts if user has excessive scopes", () => {
+  it('accepts if user has excessive scopes', () => {
+    const app = newExpressApp({
+      middleware: oauthScopes(swaggerDocument, extractor),
+      endpoint: '/'
+    })
+
     return request(app)
-      .get("/")
+      .get('/')
       .query({
-        scopes: ["dummy", "read:root", "dummier"]
+        scopes: ['dummy', 'read:root', 'dummier']
       })
-      .expect(200);
-  });
+      .expect(204)
+  })
 
-  it("rejects if user has no scopes", () => {
-    return request(app)
-      .get("/")
-      .expect(500);
-  });
+  it('rejects if user has no scopes', () => {
+    const app = newExpressApp({
+      middleware: oauthScopes(swaggerDocument, extractor),
+      endpoint: '/'
+    })
 
-  it("rejects if user has wrong scopes", () => {
     return request(app)
-      .get("/")
+      .get('/')
+      .expect(500)
+  })
+
+  it('rejects if user has wrong scopes', () => {
+    const app = newExpressApp({
+      middleware: oauthScopes(swaggerDocument, extractor),
+      endpoint: '/'
+    })
+
+    return request(app)
+      .get('/')
       .query({
-        scopes: ["write:root"]
+        scopes: ['write:root']
       })
-      .expect(500);
-  });
+      .expect(500)
+  })
 
-  it("throws an error if endpoint has no defined swagger entry", done => {
+  it('throws an error if endpoint has no defined swagger entry', done => {
+    const app = newExpressApp({
+      middleware: oauthScopes(swaggerDocument, extractor),
+      endpoint: '/wrong'
+    })
+
     request(app)
-      .get("/wrong")
+      .get('/wrong')
       .query({
-        scopes: ["write:root"]
+        scopes: ['write:root']
       })
       .expect(500)
       .then(response => {
-        assert.equal(response.body.details, "No Swagger entry for GET /wrong");
-        done();
-      });
-  });
-});
+        assert.equal(response.body.details, 'No Swagger entry for GET /wrong')
+        done()
+      })
+  })
+})
